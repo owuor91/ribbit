f16e94a7d68ad420abbb303463b31b8a7989259b
owuor91 <owuor91@gmail.com>
2015-04-25 17:00:25 +0300

    debugged user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,9 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+
+	private
+	def user_params
+		params.require(:user).permit(:avatar_url, :email, :name, :password, :password_confirmation, :username)
+	end
 end
