e79052a29ac70b96ecf8f142b6a7db43c1418996
owuor91 <owuor91@gmail.com>
2015-04-26 18:24:04 +0300

    buggy ribbit functionality

--- app/controllers/ribbits_controller.rb
+++ app/controllers/ribbits_controller.rb
@@ -1,4 +1,15 @@
 class RibbitsController < ApplicationController
+	def create
+		@ribbit = Ribbit.new(params[:ribbit])
+		@ribbit.userid = current_user.id
+
+		if @ribbit.save
+			redirect_to current_user
+		else
+			flash[:error] = "Problem!"
+			redirect_to current_user
+		end
+	end
 
 	private
 	def ribbit_params
